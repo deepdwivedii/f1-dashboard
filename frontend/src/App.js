f4d1a336c421951511847e56b27cb436731ab5cc
Deep Dwivedii <dwivedideepumesh@gmail.com>
2025-04-04 10:33:53 +0400

    multiple chnages to frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+import React from 'react';
+import LiveTiming from './components/LiveTiming';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>F1 Live Dashboard</h1>
+      <LiveTiming />
+    </div>
+  );
+}
+
+export default App;
